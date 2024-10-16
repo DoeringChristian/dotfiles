55c45febad3e1a058dac2096348fdd0be65921d7
Christian Döring <christian.doering@tum.de>
2024-10-16 21:13:11 +0200

    Added template alias

---
+++ .config/fish/functions/template.fish
@@ -0,0 +1,4 @@
+function template --wraps='copier copy ~/workspace/templates/' --description 'alias template copier copy ~/workspace/templates/'
+  copier copy ~/workspace/templates/ $argv
+        
+end
