b068d6250c6433163012414bc6a0a7fe5039b01b
Christian Döring <christian.doering@tum.de>
2025-12-26 14:31:50 +0100

    Fixed updating issue

--- sync.sh
+++ sync.sh
@@ -9,10 +9,10 @@ mkdir -p ~/.config
 mkdir -p ~/.local/bin
 mkdir -p ~/.ssh
 
-# First, use homemanager to install the required commands
+# First, install the required packages
 echo "Installing packages with Nix:"
 export NIX_CONFIG="experimental-features = nix-command flakes"
-nix profile install .#default
+nix profile remove dotfiles && nix profile install .#default
 
 # This stow comes before the others to ensure global ignore list is respected before other stow commands
 echo "Applying configs with GNU Stow:"
